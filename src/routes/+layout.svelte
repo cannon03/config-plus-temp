e7ff8c772680495a433a1b6ff9d181428bbc2b9a
shourya <shourya.s2012@gmail.com>
2025-11-14 16:18:45 +0530

    logout on absence of an access token

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { getAccessToken } from '$lib/utils/storage';
+	import { goto } from '$app/navigation';
 	let { children } = $props();
+
+	const accessToken = $derived.by(() => getAccessToken());
+
+	$effect(() => {
+		if (!accessToken || accessToken === '') {
+			goto('/');
+		}
+	});
 </script>
 
 <svelte:head>
